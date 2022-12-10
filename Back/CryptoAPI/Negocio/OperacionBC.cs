15d48a8fb43aed3813f1c5c9d8695348bd779d46
aguslopezp <agustinlopezprieto@gmail.com>
2022-12-09 23:08:14 -0300

    Agrego OperacionBC y mejora de OperacionController

---
+++ Back/CryptoAPI/Negocio/OperacionBC.cs
@@ -0,0 +1,23 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class OperacionBC
+    {
+        public List<Operacion> ObtenerOperaciones(crypto_dbContext db)
+        {
+            return db.Operaciones.ToList();
+        }
+
+        public void AgregarOperacion(crypto_dbContext db, Operacion nuevaOperacion)
+        {
+            db.Operaciones.Add(nuevaOperacion);
+            db.SaveChanges();
+        }
+    }
+}
